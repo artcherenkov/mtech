83a33f21bec598f569a562480ce795e92cd61b81
Artem Cherenkov <art.cherenkov@gmail.com>
2021-02-07 15:22:46 +0700

    Добавлен шаблон хранилища Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from "./components/app/app";
+import rootReducer from './store/reducers/root-reducer';
+import { createAPI } from './services/api';
+
+const api = createAPI();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(api)),
+  ),
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById(`root`),
 );
---
+++ src/store/reducers/app-state/app-state.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const appState = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export { appState };
