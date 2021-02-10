c4319ac391ca460151632aa166c72393d232238e
Artem Cherenkov <art.cherenkov@gmail.com>
2021-02-10 12:20:06 +0700

    Добавлен выбор статуса при помощи селекта

--- src/components/record/components/field-item/field-item.jsx
+++ src/components/record/components/field-item/field-item.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Status } from "../../../../mocks/record";
+
 const FieldItem = ({ value, recordKey, isRecordEditing, setUpdatingRecord }) => {
   const handleChange = (evt) => {
     setUpdatingRecord((prevState) => ({ ...prevState, [recordKey]: evt.target.value }));
   };
   return (
     isRecordEditing && recordKey === `status`
-      ? <input type="text" defaultValue={value} onChange={handleChange}/>
+      ? <select defaultValue={value} onChange={handleChange}>
+        {Object.entries(Status).map(([key, value]) => (
+          <option key={`option-${key}`} value={value}>{value}</option>
+        ))}
+      </select>
       : <p className="record__item-content">{value}</p>
   );
 };
