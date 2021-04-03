149f0a7c2d294d9d45fdd4a0f6ccb1585d835243
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-03 13:29:58 +0700

    Добавлен и подключен хук useDeleteRecordPopup

---
+++ src/hooks/cc-errors/useDeleteRecordPopup.js
@@ -0,0 +1,36 @@
+import React from "react";
+import NewPopup from "../../components/new-popup/new-popup";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { getRecordToDeleteId } from "../../store/reducers/cc-errors/selectors";
+import {
+  deleteRecord,
+  setRecordToDelete,
+} from "../../store/reducers/cc-errors/actions";
+
+export const useDeleteRecordPopup = () => {
+  const dispatch = useDispatch();
+  const recordToDeleteId = useSelector(getRecordToDeleteId, shallowEqual);
+
+  const handleRecordDelete = () => dispatch(deleteRecord(recordToDeleteId));
+  const onClosePopup = () => dispatch(setRecordToDelete(-1));
+
+  return (
+    <NewPopup onOutsideClick={onClosePopup}>
+      <div className="popup__controls">
+        <button className="popup__button_close" onClick={onClosePopup} />
+      </div>
+      <h2 className="popup__header">Вы уверены, что хотите удалить запись?</h2>
+      <div className="popup__controls">
+        <button
+          className="popup__button popup__button_danger"
+          onClick={handleRecordDelete}
+        >
+          Удалить
+        </button>
+        <button className="popup__button" onClick={onClosePopup}>
+          Отмена
+        </button>
+      </div>
+    </NewPopup>
+  );
+};
--- src/pages/cc-errors/cc-errors.jsx
+++ src/pages/cc-errors/cc-errors.jsx
@@ -1,38 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect, shallowEqual, useDispatch, useSelector } from "react-redux";
+import { connect, shallowEqual, useSelector } from "react-redux";
 
 import Header from "../../components/header/header";
 import Lock from "../../components/lock/lock";
 import { getIsAuth } from "../../store/reducers/app-user/selectors";
 import { useRecordsSelector } from "../../hooks/cc-errors/useRecordsSelector";
 import { useTable } from "../../hooks/cc-errors/useTable";
-import Popup from "../../components/popup/popup";
 import {
   getIsDeletePopupShown,
   getIsRecordPopupShown,
-  getRecordToDeleteId,
 } from "../../store/reducers/cc-errors/selectors";
-import {
-  deleteRecord,
-  setRecordToDelete,
-} from "../../store/reducers/cc-errors/actions";
 import { useRecordPopup } from "../../hooks/cc-errors/useRecordPopup";
+import { useDeleteRecordPopup } from "../../hooks/cc-errors/useDeleteRecordPopup";
 
 const CCErrorsPage = (props) => {
-  const dispatch = useDispatch();
-
   const records = useRecordsSelector();
 
-  const recordToDeleteId = useSelector(getRecordToDeleteId, shallowEqual);
   const isDeletePopupShown = useSelector(getIsDeletePopupShown, shallowEqual);
   const isRecordPopupShown = useSelector(getIsRecordPopupShown, shallowEqual);
 
   const table = useTable(records);
   const recordPopup = useRecordPopup();
-
-  const handleRecordDelete = () => dispatch(deleteRecord(recordToDeleteId));
-  const handleCancelBtnClick = () => dispatch(setRecordToDelete(-1));
+  const deleteRecordPopup = useDeleteRecordPopup();
 
   return (
     <>
@@ -42,12 +32,7 @@ const CCErrorsPage = (props) => {
       ) : (
         <Lock />
       )}
-      {isDeletePopupShown && (
-        <Popup
-          handleRecordDelete={handleRecordDelete}
-          handleCancelBtnClick={handleCancelBtnClick}
-        />
-      )}
+      {isDeletePopupShown && deleteRecordPopup}
       {isRecordPopupShown && recordPopup}
     </>
   );
