fc9ce6e23d800a57b331df2641512ab431a039db
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-20 18:00:11 +0700

    Исправлена backend-ссылка

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = `https://1699de9eb372.ngrok.io`;
+const BACKEND_URL = `https://admin.yclamo.ru`;
 // const BACKEND_URL = `http://localhost:50110`;
 const REQUEST_TIMEOUT = 10000;
 
